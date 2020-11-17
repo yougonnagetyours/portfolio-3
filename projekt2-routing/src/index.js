691f59c99f7f77fd495211c3ce91a0a82edb5ef3
Assembly13 <assemblyinstruction13@gmail.com>
2020-11-17 14:12:04 +0000

    first commit ever

--- projekt2-routing/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './layouts/App';
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-
--- projekt2-routing/src/pages/ErrorPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ErrorPage = () => {
-    return (
-        <div>Nie ma takiej strony</div>
-    );
-}
-
-export default ErrorPage;
